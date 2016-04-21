99b594fb25faa934b53b32e835d3bfb1dfd22e92
wisidiggidy <igk2718@rit.edu>
2016-04-21 08:31:38 -0400

    Adding the UI shell file

---
+++ java/MySQLUI.java
@@ -0,0 +1,10 @@
+public class MySQLUI {
+	
+	public mySQLUI() {
+		//build the frame and main panel
+		JFrame frame = new JFrame("Heist Game");
+		JPanel main = new JPanel();
+		main.setLayout(new BorderLayout());
+	}
+	
+}
